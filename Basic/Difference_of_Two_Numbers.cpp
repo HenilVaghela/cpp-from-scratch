92943e9a32cd71de00e9c42f24ed4036ad0f5976
Henil Vaghela <vaghelahenil7@gmail.com>
2025-10-31 22:11:45 +0530

    C++ program to calculate difference of two numbers

---
+++ Basic/Difference_of_Two_Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    double difference = a - b;
+    cout << "Difference = " << difference << endl;
+
+    return 0;
+}
