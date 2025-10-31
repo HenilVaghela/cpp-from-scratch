6513106d85d4313e21881012daf69f517a2a93cd
Henil Vaghela <vaghelahenil7@gmail.com>
2025-10-31 22:12:30 +0530

    C++ program to calculate product of two numbers

---
+++ Basic/Product_of_Two_Numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    double product = a * b;
+    cout << "Product = " << product << endl;
+
+    return 0;
+}
